3bcbf8746a7f29a218f3b922a2c0214b10b4ccb2
winstonwang <wwang@lcs.on.ca>
2021-12-02 09:52:10 -0500

    added selection statement to provide feedback about the uses guesses

--- GuessingGame/ContentView.swift
+++ GuessingGame/ContentView.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var currentGuess: Double = 50.0
+    
+    let target = Int.random(in: 1...100)
+    
+    @State var feedback = "" //no hint in the game
+    
     var body: some View {
         VStack{
-            Slider(value: .constant(64.0),
+            Slider(value: $currentGuess,
                    in: 0.0...100.0,
                    step: 1.0,
                    label: {
-                Text("Opacity")
             },
                    minimumValueLabel: {
                 Text("0")
@@ -22,15 +28,35 @@ struct ContentView: View {
                    maximumValueLabel: {
                 Text("100")
             })
-            Text("64")
+            Text("\(String(format: "%.0f",currentGuess))")
                 .font(.largeTitle)
                 .bold()
             Button(action: {
+                
+                // make the users guess into an integer
+                let currentGuessAsInteger = Int(currentGuess)
+                
+                if currentGuessAsInteger < target{
+                    feedback = "Guess higher next time!"
+                } else if currentGuessAsInteger > target{
+                    feedback = "Guess lower next time!"
+                } else {
+                    feedback = "You Got It!!!"
+                }
+                
+                
                 print("Button was pressed")
             }, label: {
                 Text("Press me")
             })
                 .buttonStyle(.automatic)
+                .padding(.top, 30)
+            
+            Text("The super secret target is \(target)")
+            
+            Text(feedback)
+                .italic()
+                .padding()
             
             Spacer()
         }
