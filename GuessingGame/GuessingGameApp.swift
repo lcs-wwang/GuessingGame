1349d0d010b0dc0dd8ae2614ed2346be9d06e498
winstonwang <wwang@lcs.on.ca>
2021-12-02 09:09:23 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Winston Wang on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
